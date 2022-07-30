b73cbf22cb91b34ac3250a9288df5c193aeacb6e
ks12b0000 <ks12b0000@gmail.com>
2022-07-31 03:46:20 +0900

    관광지 api db에 저장하는 코드 추가

--- src/main/java/kr/co/intrip/tourist/dao/TouristDAO.java
+++ src/main/java/kr/co/intrip/tourist/dao/TouristDAO.java
@@ -1,8 +1,14 @@
 package kr.co.intrip.tourist.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.intrip.login_signup.dto.MemberDTO;
+import kr.co.intrip.tourist.dto.ApiDTO;
+import kr.co.intrip.tourist.dto.TouristDTO;
 
 @Repository
 public class TouristDAO {
@@ -10,4 +16,9 @@ public class TouristDAO {
 	@Autowired
 	SqlSession sqlSession;
 	
+	// 관광지 api db에 저장용
+	public void touristadd(ArrayList<ApiDTO> list) {
+		sqlSession.insert("mapper.tourist.touristadd", list);
+		
+	}
 }
